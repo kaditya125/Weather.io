fa7c66dcfdd2bd7e542fb7f33a12ea87e00235e7
kaditya125 <80103410+kaditya125@users.noreply.github.com>
2024-02-04 21:59:55 +0530

    C

--- assets/js/app.js
+++ assets/js/app.js
@@ -61,7 +61,7 @@ searchField.addEventListener("input", function () {
                         <span class="m-icon">location_on</span>
                         <div>
                             <p class="item-title">${name}</p>
-                            <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+                            <p class="label-2 item-subtitle">${name}, ${state || ""}, ${country}</p>
                         </div>
                         <a href="#/weather?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
                      `;
